50aa337a75fb7a6cac11e4e91bf09888154fb110
Chrisp6349 <chrisparrish1@btinternet.com>
2026-07-22 05:38:10 +0100

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -7,11 +7,12 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import {
-  getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut
+  getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut,
+  updatePassword, reauthenticateWithCredential, EmailAuthProvider
 } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import {
   getFirestore, doc, getDoc, setDoc, updateDoc, deleteDoc,
-  collection, getDocs, query, where, orderBy, onSnapshot, serverTimestamp
+  collection, getDocs, addDoc, query, where, orderBy, limit, onSnapshot, serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
 import { firebaseConfig } from "./firebase-config.js";
@@ -22,6 +23,7 @@ export const db = getFirestore(app);
 
 export {
   onAuthStateChanged, signInWithEmailAndPassword, signOut,
+  updatePassword, reauthenticateWithCredential, EmailAuthProvider,
   doc, getDoc, setDoc, updateDoc, deleteDoc,
-  collection, getDocs, query, where, orderBy, onSnapshot, serverTimestamp
+  collection, getDocs, addDoc, query, where, orderBy, limit, onSnapshot, serverTimestamp
 };
